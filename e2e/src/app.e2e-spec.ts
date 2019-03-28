0fb54b58e9c74939eb3ae2035ea01beb1f94ee19
haopo <haopoboy@gmail.com>
2019-03-28 23:03:47 +0800

    Fix failed tests by renaming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to erp3-fast!');
+    expect(page.getTitleText()).toEqual('Welcome to Fast MD!');
   });
 
   afterEach(async () => {
