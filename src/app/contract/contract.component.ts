8493b1af36534c8409eac7929b8a0f8d8284674b
haopo <haopoboy@gmail.com>
2019-04-03 09:48:55 +0800

    Fix missing base URL

--- src/app/contract/contract.component.ts
+++ src/app/contract/contract.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-contract",
@@ -23,7 +24,11 @@ export class ContractComponent implements OnInit {
 
   async findOne(id: string) {
     const page: any = await this.http
-      .get(`/query/v2/contractProjectV2?onlyShowMine=false&systemId=${id}`)
+      .get(
+        `${
+          environment.apiBaseUrl
+        }/query/v2/contractProjectV2?onlyShowMine=false&systemId=${id}`
+      )
       .toPromise();
 
     page.content.forEach(row => {
